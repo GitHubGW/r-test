917e55c51af6892b637035d25042e99cbf0a58a3
GW <kowonp@gmail.com>
2021-09-02 00:03:34 +0900

    Make Express server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+app.get("/", () => console.log("Home"));
+
+const handleListening = () => {
+  console.log(`🔥 http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
