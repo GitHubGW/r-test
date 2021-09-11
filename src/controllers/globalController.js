58024f4625b13369a8633640e0ec63c987bda0b9
GW <kowonp@gmail.com>
2021-09-11 22:05:40 +0900

    Iteration, Mixin in Pug

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -1,4 +1,33 @@
-export const handleHome = (req, res) => res.render("home", { title: "Home" });
+export const handleHome = (req, res) => {
+  const videos = [
+    {
+      id: 1,
+      title: "Video#1",
+      rating: 5,
+      comments: 2,
+      createdAT: "2mins ago",
+      views: 60,
+    },
+    {
+      id: 2,
+      title: "Video#2",
+      rating: 25,
+      comments: 2,
+      createdAT: "2mins ago",
+      views: 30,
+    },
+    {
+      id: 3,
+      title: "Video#3",
+      rating: 45,
+      comments: 12,
+      createdAT: "2mins ago",
+      views: 20,
+    },
+  ];
+
+  res.render("home", { title: "Home", videos });
+};
 export const handleJoin = (req, res) => res.send("Join");
 export const handleLogin = (req, res) => res.send("Login");
 export const handleSearch = (req, res) => res.send("Search");
